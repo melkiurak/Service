9612280cca2374f734413a5d36fd2c213820f13d
melkiurak <melkiurak3209@gmail.com>
2025-11-19 16:23:07 +0200

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Main from "@/components/features/Main/Main"
 import SideBar from "@/components/features/SideBar/SideBar"
 export default function home() {
     return(
         <div className="flex h-full">
             <SideBar/>
+            <Main/>
         </div>
     )
 }
